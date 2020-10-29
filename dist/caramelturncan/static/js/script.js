cf8ce46b9df380afb9432d850d31d6bc1e59d90a
danielesteves <danielmorada@gmail.com>
2020-10-29 18:38:07 -0300

    changed gif to video

--- dist/caramelturncan/static/js/script.js
+++ dist/caramelturncan/static/js/script.js
@@ -4,6 +4,7 @@
   items: [{ name: 'Kittens' }]
 });
 */
+firebase.analytics();
 
 document.addEventListener('click', function (event) {
 
@@ -21,3 +22,16 @@ document.addEventListener('click', function (event) {
   });
 
 }, false);
+
+function initFingerprintJS() {
+	FingerprintJS.load().then(fp => {
+		// The FingerprintJS agent is ready.
+		// Get a visitor identifier when you'd like to.
+		fp.get().then(result => {
+			// This is the visitor identifier:
+			const visitorId = result.visitorId;
+			console.log(visitorId);
+			firebase.analytics().setUserProperties({deviceid: visitorId});
+		});
+	});
+}
