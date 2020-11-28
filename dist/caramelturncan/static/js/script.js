696998f4adb1a716c4206322f5586daf106a35c8
danielesteves <danielmorada@gmail.com>
2020-11-28 11:33:22 -0300

    added hashtag ling

--- dist/caramelturncan/static/js/script.js
+++ dist/caramelturncan/static/js/script.js
@@ -10,12 +10,7 @@ document.addEventListener('click', function (event) {
 
 	// If the clicked element doesn't have the right selector, bail
 	if (!event.target.matches('#doguinho_img')) return;
-
-	// Don't follow the link
 	event.preventDefault();
-
-	// Log the clicked element in the console
-	console.log(event.target);
   firebase.analytics().logEvent('select_content', {
     content_type: 'image',
     content_id: 'doguinho_img'
